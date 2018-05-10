9f56e5d885226a5eb5138e9c0438c61069565d55
Emilio Nguyen <desmondanh@users.noreply.github.com>
2018-05-10 08:28:47 +0700

    create some model classes note: implement override save method

--- Project/django_settings/django_settings/urls.py
+++ Project/django_settings/django_settings/urls.py
@@ -17,5 +17,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    # path('', include('watermark.urls')),
     path('admin/', admin.site.urls),
 ]
--- Project/django_settings/watermark/models.py
+++ Project/django_settings/watermark/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Song(models.Model):
+    name = models.CharField(null=True, blank=True, max_length=100)
+    srcLink = models.CharField(null=True, blank=True, max_length=200)
+    genre = models.CharField(null=True, blank=True, max_length=100)
+    author = models.CharField(null=True, blank=True, max_length=100)
+    # price = models.FloatField(default=0)
+
+    class Meta:
+        db_table = 'watermark_song'
+
+    def __str__(self):
+        if self.author is None:
+            self.author = " "
+        return self.author + " - " + self.name
+
+    # def save(self, *args, **kargs):
+    #     if self.link is None:
+    #         self.link = "https://docs.google.com/uc?export=open&id="+self.id
+    #     super(Song,self).save(*args, **kargs)
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(blank=True, null=True)
+    songs = models.ManyToManyField(Song)
+
+    def __str__(self):
+        return self.name + " - " + self.email
