ccc33df92df107fdd0a49f3e14db82716e247510
Emilio Nguyen <desmondanh@users.noreply.github.com>
2018-05-18 11:39:28 +0700

    delete unused javascript and edit play music js for mac fix fix my song for superuser is none

--- Project/django_settings/watermark/static/base.js
+++ Project/django_settings/watermark/static/base.js
@@ -1,36 +1,4 @@
 $(function(){
-    $('audio').on('play', ()=>{
-        $("audio").not(this).each(function(index, audio) {
-            audio.pause();
-            var id = audio.currentSrc.split("=").pop();
-            var unselectedRow = document.getElementById(id);
-            $(unselectedRow).removeClass('active');
-        });
-    });
-    var login = $('#login');
-    var signup = $('#signup');
-    if(login.length){
-        login.addClass('disabled');
-    }else if(signup.length){
-        signup.addClass('disabled');
-    }
-    $('form div input').blur(()=>{
-        if(this.value==""){
-            if(login.length){
-                $('#login').removeClass('disabled');
-                $('#login').addClass('disabled');
-            }else if(signup.length){
-                $('#signup').removeClass('disabled');
-                $('#signup').addClass('disabled');
-            }
-        }else{
-            if(login.length){
-                $('#login').removeClass('disabled');
-            }else if(signup.length){
-                $('#signup').removeClass('disabled');
-            }
-        }
-    });
     var message = $('.messages');
     if(message){
         var decrBool = message.find('#watermark');
@@ -52,4 +20,12 @@ function playEvent(element){
     var id = element.currentSrc.split("=").pop();
     var selectedRow = document.getElementById(id);
     $(selectedRow).addClass('active');
+    $("audio").each((index,audio)=>{
+        if(audio!=element){
+            audio.pause();
+            var id = audio.currentSrc.split("=").pop();
+            var unselectedRow = document.getElementById(id);
+            $(unselectedRow).removeClass('active');
+        }
+    });
 }
